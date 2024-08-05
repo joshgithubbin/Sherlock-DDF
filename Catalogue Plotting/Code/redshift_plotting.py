502d4de6c5ac851b03a5ff2e9412506b2efe3b93
joshgithubbin <jwestonstem@gmail.com>
2024-08-05 16:21:20 +0100

    Updating histograms

--- Catalogue Plotting/Code/redshift_plotting.py
+++ Catalogue Plotting/Code/redshift_plotting.py
@@ -7,10 +7,11 @@ Created on Wed Jul  3 10:08:47 2024
 """
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 
 def plot_zhist(field,z,labels="",savefig=False,savename="field"):
    
-    plt.hist(z, label=labels, bins=20, edgecolor='black', alpha=0.7)
+    plt.hist(z, label=labels, bins=np.arange(0,7.5,0.05).tolist(), edgecolor='black', alpha=0.7)
     
     plt.legend()
     plt.title(f'{field} Redshifts',fontweight='bold')
